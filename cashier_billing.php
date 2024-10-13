48690d3738a2c02554e9c86a1394eb6250ef83b8
edriccoder <limpiado098@gmail.com>
2024-10-13 17:36:36 +0800

    Update cashier_billing.php

--- cashier_billing.php
+++ cashier_billing.php
@@ -224,7 +224,7 @@ foreach ($users as $user) {
          <div class="card">
             <div class="card-header pb-0 px-3">
                <h6 class="mb-0">Billing and Payments</h6>
-               <button class='btn btn-primary editBtn' onclick='generateReport()'>Generate Reports</button>
+               <button class='btn btn-primary editBtn' onclick='generateReport()'>Generate Reports Today</button>
             </div>
             <div class="card-body pt-4 p-3">
                <ul class="list-group">
