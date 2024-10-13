6727f2eb79fb24f1b7d665105ba2b02e200e5037
edriccoder <limpiado098@gmail.com>
2024-10-13 20:15:56 +0800

    Update cashier_dashboard.php

--- cashier_dashboard.php
+++ cashier_dashboard.php
@@ -1,8 +1,13 @@
-<!DOCTYPE html>
 <?php
 // Include database connection
 include 'conn.php';
-
+session_start();
+if (!isset($_SESSION['username']) || !isset($_SESSION['user_id'])) {
+   // Redirect to login page if session variables are not set
+   header("Location: index.php");
+   exit();
+   
+}
 $username = htmlspecialchars($_SESSION['username']);
 $user_id = htmlspecialchars($_SESSION['user_id']);
 
