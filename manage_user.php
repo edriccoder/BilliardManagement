bb854371144c2dee4c00aee6eb413ffa44a87f08
edriccoder <limpiado098@gmail.com>
2024-10-18 22:23:34 +0800

    Update manage_user.php

--- manage_user.php
+++ manage_user.php
@@ -691,36 +691,6 @@
             modal.find('#editCashierUsername').val(username);
          });
 
-         $('#editUserModal').on('show.bs.modal', function (event) {
-            var button = $(event.relatedTarget); // Button that triggered the modal
-            var userId = button.data('userid');
-            var name = button.data('name');
-            var email = button.data('email');
-            var username = button.data('username');
-
-            // Get the modal elements
-            var modal = $(this);
-            modal.find('#editUserId').val(userId);
-            modal.find('#editUserName').val(name);
-            modal.find('#editUserEmail').val(email);
-            modal.find('#editUserUsername').val(username);
-         });
-
-         // Fill the cashier modal with data
-         $('#editCashierModal').on('show.bs.modal', function (event) {
-            var button = $(event.relatedTarget); // Button that triggered the modal
-            var userId = button.data('userid');
-            var name = button.data('name');
-            var email = button.data('email');
-            var username = button.data('username');
-
-            // Get the modal elements
-            var modal = $(this);
-            modal.find('#editCashierId').val(userId);
-            modal.find('#editCashierName').val(name);
-            modal.find('#editCashierEmail').val(email);
-            modal.find('#editCashierUsername').val(username);
-         });
       </script>
 
 
