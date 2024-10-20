4c57954cb238da576814663aad98c90620aa7591
edriccoder <limpiado098@gmail.com>
2024-10-20 20:39:45 +0800

    Update admin_dashboard.php

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -118,7 +118,15 @@ try {
                      </div>
                      <span class="nav-link-text ms-1">Inventory Management</span>
                   </a>
-               </li> 
+               </li>
+            <li class="nav-item">
+               <a class="nav-link text-white " href="admin_announcement.php">
+                  <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                     <i class="material-icons opacity-10">announce</i>
+                  </div>
+                  <span class="nav-link-text ms-1">Manage Announcement</span>
+               </a>
+            </li>
             <li class="nav-item">
                <a class="nav-link text-white " href="admin_booking.php">
                   <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
