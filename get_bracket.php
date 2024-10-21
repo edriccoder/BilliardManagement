14ffdaa4da94d4652bdf4c0b1b74fdb27013b95a
edriccoder <limpiado098@gmail.com>
2024-10-21 20:14:07 +0800

    Update get_bracket.php

--- get_bracket.php
+++ get_bracket.php
@@ -1,5 +1,8 @@
 <?php
 // get_bracket.php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include 'conn.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['tournament_id'])) {
