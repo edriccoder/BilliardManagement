32dae72a362b2152f0421daa036eb03f02d55e95
edriccoder <limpiado098@gmail.com>
2024-10-21 19:54:38 +0800

    Update manage_tournament.php

--- manage_tournament.php
+++ manage_tournament.php
@@ -29,6 +29,7 @@
       <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
       <!-- Material Icons -->
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
       <!-- CSS Files -->
       <link id="pagestyle" href="./assets/css/material-dashboard.css?v=3.1.0" rel="stylesheet" />
       <!-- Nepcha Analytics (nepcha.com) -->
